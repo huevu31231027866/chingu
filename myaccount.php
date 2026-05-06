a8e11f37db48a11003df31410a2ad6fe27e6ab48
huevu31231027866 <huevu.31231027866@st.ueh.edu.vn>
2026-05-06 13:20:57 +0700

    Update myaccount.php

--- myaccount.php
+++ myaccount.php
@@ -7,20 +7,8 @@ function custom_final_menu_reorder( $items ) {
         'orders'          => 'Đơn hàng',             
         'edit-address'    => 'Địa chỉ',              
         'payment-methods' => 'Phương thức thanh toán', 
-        'lost-password'   => 'Quên mật khẩu', // Trả lại endpoint chuẩn            
+          
     );
 
     return $new_items;
 }
-
-// 2. Cách cuối cùng: Dùng Javascript để gán link nếu PHP vẫn bị "lỳ"
-add_action( 'wp_footer', function() {
-    if ( ! is_account_page() ) return;
-    ?>
-    <script>
-        document.querySelectorAll('#my-account-nav a').forEach(a => {
-            if (a.innerText.includes('Quên mật khẩu')) a.href = '<?php echo home_url("/my-account/lost-password/"); ?>';
-        });
-    </script>
-    <?php
-}, 999 );
